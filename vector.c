ed44ef1a0dd7b1f80b09b43789b9e1794dbbfa2b
NimeMarks <161144496+NimeMarks@users.noreply.github.com>
2024-11-18 02:48:19 +0530

    Update vector.c

--- vector.c
+++ vector.c
@@ -2,33 +2,26 @@
 #define MAX_NODES 10
 #define INF 9999
 
-typedef struct {
-int cost[MAX_NODES];
-int from[MAX_NODES];
-} DistanceTable;
-
 void distanceVectorRouting(int nodes, int costMatrix[MAX_NODES][MAX_NODES]) {
-DistanceTable table[MAX_NODES];
+int cost[MAX_NODES][MAX_NODES], from[MAX_NODES][MAX_NODES];
 int i, j, k;
 
-// Initialize distance table
 for (i = 0; i < nodes; i++) {
 for (j = 0; j < nodes; j++) {
-table[i].cost[j] = costMatrix[i][j];
-table[i].from[j] = j;
+cost[i][j] = costMatrix[i][j];
+from[i][j] = j;
 }
 }
 
 int updated;
 do {
 updated = 0;
-
 for (i = 0; i < nodes; i++) {
 for (j = 0; j < nodes; j++) {
 for (k = 0; k < nodes; k++) {
-if (table[i].cost[j] > costMatrix[i][k] + table[k].cost[j]) {
-table[i].cost[j] = table[i].cost[k] + table[k].cost[j];
-table[i].from[j] = k;
+if (cost[i][j] > cost[i][k] + cost[k][j]) {
+cost[i][j] = cost[i][k] + cost[k][j];
+from[i][j] = k;
 updated = 1;
 }
 }
@@ -36,12 +29,11 @@ updated = 1;
 }
 } while (updated);
 
-// Print final routing table
 for (i = 0; i < nodes; i++) {
 printf("Routing Table for Node %d:\n", i + 1);
 printf("Dest\tNext Hop\tCost\n");
 for (j = 0; j < nodes; j++) {
-printf("%d\t%d\t\t%d\n", j + 1, table[i].from[j] + 1, table[i].cost[j]);
+printf("%d\t%d\t\t%d\n", j + 1, from[i][j] + 1, cost[i][j]);
 }
 printf("\n");
 }
@@ -59,7 +51,7 @@ for (i = 0; i < nodes; i++) {
 for (j = 0; j < nodes; j++) {
 scanf("%d", &costMatrix[i][j]);
 if (i == j) {
-costMatrix[i][j] = 0;
+costMatrix[i][j] = 0; 
 }
 }
 }
