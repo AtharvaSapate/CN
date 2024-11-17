9afdfceeeb7f147f7fb59f0b926026e8ac66204c
NimeMarks <161144496+NimeMarks@users.noreply.github.com>
2024-11-18 03:02:09 +0530

    Update stop.c

--- stop.c
+++ stop.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include<stdbool.h>
 #include <unistd.h>
 #include <time.h>
 
@@ -10,26 +10,25 @@ return rand() % 2 == 0;
 
 int main() {
 int frames[] = {1, 2, 3};
-int total_frames = sizeof(frames) / sizeof(frames[0]);
 int sequence_number = 0;
 
 srand(time(0));
 
-for (int i = 0; i < total_frames; i++) {
+for (int i = 0; i < 3; i++) {
 bool ack_received = false;
 while (!ack_received) {
 printf("Sending frame %d with sequence number %d\n", frames[i], sequence_number);
 sleep(1);
 ack_received = receive_ack();
 if (ack_received) {
-printf("ACK received for sequence number %d\n\n", sequence_number);
+printf("ACK received\n");
 sequence_number = 1 - sequence_number;
 } else {
-printf("ACK not received. Retransmitting frame %d...\n\n", frames[i]);
+printf("ACK not received. Retransmitting...\n");
 }
 }
 }
 
-printf("All frames sent successfully!\n");
+printf("All frames sent!\n");
 return 0;
 }
